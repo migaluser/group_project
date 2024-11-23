73949f9482fe3f6fd1102b099ee1f28b03f4d400
migaluser <muhaylolubka@gmail.com>
2024-11-23 16:56:18 +0200

    changed code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+var stores = [
+    { name: "Магазин 1", location: [50.4501, 30.5236], address: "ул. Примерная, 1" },
+    { name: "Магазин 2", location: [50.4511, 30.5246], address: "ул. Примерная, 2" }
+];
+
+stores.forEach(function(store) {
+    L.marker(store.location).addTo(map)
+        .bindPopup("<b>" + store.name + "</b><br>Адрес: " + store.address);
+});
